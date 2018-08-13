baf3a045541f2f6e718881fddadd624fad3cca8e
David Riccitelli <david@insideout.io>
2018-08-13 16:27:06 +0300

    bump to 1.0.0-rc4

--- wordlift-entity-redirect.php
+++ wordlift-entity-redirect.php
@@ -15,7 +15,7 @@
  * Plugin Name:       WordLift Entity Redirect
  * Plugin URI:        https://wordlift.io
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0-rc1
+ * Version:           1.0.0-rc4
  * Author:            David Riccitelli
  * Author URI:        https://wordlift.io
  * License:           GPL-2.0+
@@ -34,7 +34,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'WL_ENTITY_REDIRECT_VERSION', '1.0.0-rc1' );
+define( 'WL_ENTITY_REDIRECT_VERSION', '1.0.0-rc4' );
 
 /**
  * The code that runs during plugin activation.

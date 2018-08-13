b64ace4063dcce2103acfad43f1c628a8a3b6da7
David Riccitelli <david@insideout.io>
2018-08-13 16:25:18 +0300

    bump to 1.0.0-rc1

--- includes/class-wordlift-entity-redirect.php
+++ includes/class-wordlift-entity-redirect.php
@@ -67,8 +67,8 @@ class Wordlift_Entity_Redirect {
 	 * @since    1.0.0
 	 */
 	public function __construct() {
-		if ( defined( 'PLUGIN_NAME_VERSION' ) ) {
-			$this->version = PLUGIN_NAME_VERSION;
+		if ( defined( 'WL_ENTITY_REDIRECT_VERSION' ) ) {
+			$this->version = WL_ENTITY_REDIRECT_VERSION;
 		} else {
 			$this->version = '1.0.0';
 		}

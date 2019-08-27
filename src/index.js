c774cd15e934b8c5eb0a1c46ea9f2d00ac5b6c2a
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2019-08-27 23:19:08 +0530

    feat: add gutenberg ToggleControl for redirect

--- src/index.js
+++ src/index.js
@@ -1,13 +1,61 @@
 const { registerPlugin } = wp.plugins;
 const { PluginPostStatusInfo } = wp.editPost;
+const { TextControl, ToggleControl, FormToggle } = wp.components;
+const { withSelect, withDispatch } = wp.data;
+const { withState } = wp.compose;
+const { __ } = wp.i18n;
 
+registerPlugin( 'my-plugin-sidebar', {
+	render() {
 
-const PluginPostStatusInfoTest = () => {
-	return(
-		<PluginPostStatusInfo>
-			<p>Post Status Info SlotFill</p>
-		</PluginPostStatusInfo>
-	)
-}
+		const mapSelectToProps = select => {
+			return {
+				metaFieldValue: select( 'core/editor' ).getEditedPostAttribute( 'meta' )[ 'sid_toggle' ],
+			}
+		}
+		
+		const mapDispatchToProps = ( dispatch ) => {
+			return {
+				setMetaFieldValue: function( value ) {
+					dispatch( 'core/editor' ).editPost(
+						{
+							meta: {
+								sid_toggle: value
+							}
+						}
+					);
+				}
+			}
+		}
+		
+		const MetaBlockField = ( props ) => {
+			return (
+				<ToggleControl
+					checked={ props.metaFieldValue }
+					onChange={ content => {
+						props.setMetaFieldValue( content )
+					} }
+				/>
+			);
+		}
 
-registerPlugin( 'post-status-info-test', { render: PluginPostStatusInfoTest } );
+		const MyFormToggle = withState( {
+			checked: true,
+		} )( ( { checked, setState } ) => (
+			<FormToggle 
+				checked={ checked }
+				onChange={ () => setState( state => ( { checked: ! state.checked } ) ) } 
+			/>
+		) );
+		
+		const MetaBlockFieldWithData = withSelect( mapSelectToProps )( MetaBlockField );
+		const MetaBlockFieldWithDataAndActions = withDispatch( mapDispatchToProps )( MetaBlockFieldWithData );
+
+		return (
+			<PluginPostStatusInfo>
+				<label>{ __( 'Entity Redirect', 'wordlift-entity-redirect' ) }</label>
+				<MetaBlockFieldWithDataAndActions />
+			</PluginPostStatusInfo>	
+		);
+	}
+} );

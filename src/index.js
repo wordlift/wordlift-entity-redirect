cd93a05261a3d3cee09d70e4cf4897ed72fa3040
David Riccitelli <david@wordlift.io>
2020-03-09 10:06:24 +0100

    bump to 1.1.0

--- src/index.js
+++ src/index.js
@@ -10,29 +10,29 @@ const { __ } = wp.i18n;
  *
  * @param {function} select
  */
-const mapSelectToProps = ( select ) => {
+const mapSelectToProps = select => {
 	return {
-		metaFieldValue: select( 'core/editor' ).getEditedPostAttribute( 'meta' )[ '_wl_entity_redirect_enabled' ],
-	}
-}
+		metaFieldValue: select("core/editor").getEditedPostAttribute("meta")[
+			"_wl_entity_redirect_enabled"
+		]
+	};
+};
 
 /**
  * Updates the meta value.
  *
  * @param {function} dispatch
  */
-const mapDispatchToProps = ( dispatch ) => {
+const mapDispatchToProps = dispatch => {
 	return {
-		setMetaFieldValue: function( value ) {
-			dispatch( 'core/editor' ).editPost(
-				{
-					meta: {
-						_wl_entity_redirect_enabled: value ? 'yes' : 'no'
-					}
+		setMetaFieldValue: function(value) {
+			dispatch("core/editor").editPost({
+				meta: {
+					_wl_entity_redirect_enabled: value ? "yes" : "no"
 				}
-			);
+			});
 		}
-	}
+	};
 };
 
 /**
@@ -40,29 +40,36 @@ const mapDispatchToProps = ( dispatch ) => {
  *
  * @param {object} props Props passed by mapSelectToProps.
  */
-const EntityRedirectSwitch = ( props ) => {
-
+const EntityRedirectSwitch = props => {
 	return (
 		<FormToggle
-			checked={ 'yes' === props.metaFieldValue }
-			onChange={ () => props.setMetaFieldValue( ! ( 'yes' === props.metaFieldValue ) ) }
+			checked={"no" !== props.metaFieldValue}
+			onChange={() =>
+				props.setMetaFieldValue(!("yes" === props.metaFieldValue))
+			}
 		/>
 	);
 };
 
-const EntityRedirectSwitchWithData           = withSelect( mapSelectToProps )( EntityRedirectSwitch );
-const EntityRedirectSwitchWithDataAndActions = withDispatch( mapDispatchToProps )( EntityRedirectSwitchWithData );
+const EntityRedirectSwitchWithData = withSelect(mapSelectToProps)(
+	EntityRedirectSwitch
+);
+const EntityRedirectSwitchWithDataAndActions = withDispatch(mapDispatchToProps)(
+	EntityRedirectSwitchWithData
+);
 
 /**
  * Registers a new control in the Plugin Post slot fill and renders it.
  */
-registerPlugin( 'wer-entity-redirect-toggle', {
+registerPlugin("wer-entity-redirect-toggle", {
 	render() {
 		return (
 			<PluginPostStatusInfo>
-				<label>{ __( 'Entity Redirect', 'wordlift-entity-redirect' ) }</label>
+				<label>
+					{__("Entity Redirect", "wordlift-entity-redirect")}
+				</label>
 				<EntityRedirectSwitchWithDataAndActions />
-			</PluginPostStatusInfo>	
+			</PluginPostStatusInfo>
 		);
 	}
-} );
+});
